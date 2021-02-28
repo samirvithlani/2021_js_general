681d1b36063994b49af7b85df8046e6452a9803b
=samirvithlani <=samir.vithlani83955@gmail.com>
2021-02-28 17:44:29 +0530

    js lec 1

---
+++ js/index1.js
@@ -0,0 +1,18 @@
+//console...  log function
+
+///ES2015 let const
+
+var x = 100;
+var f = 20.20;
+var user_name = "raj";
+var falg = false
+const price = 200;
+let p = 250;
+console.log("hello")
+console.log("value of x =", x)
+console.log("value of f =", f)
+console.log("name= ", user_name);
+console.log("flag = ", falg)
+//price = 500;
+console.log(price)
+console.log(p)
